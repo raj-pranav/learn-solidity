580c76c7bbb1cd4d1b7591f2c4e3ef3b1a8058ed
Pranav Raj <48473708+raj-pranav@users.noreply.github.com>
2022-01-14 22:17:09 +0530

    Create 2_added_state_var.sol

---
+++ contracts/2_added_state_var.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT-License
+
+pragma solidity >0.5.0 <=0.9.0;
+
+contract Organization {
+    uint16 num_of_emp = 100; // This will be stored on blockchain and costs money
+}
