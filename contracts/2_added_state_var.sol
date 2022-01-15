abd6c6230c5460fac658c24f8c25f5d017481c62
Pranav Raj <48473708+raj-pranav@users.noreply.github.com>
2022-01-15 13:26:07 +0530

    Update 2_added_state_var.sol

--- contracts/2_added_state_var.sol
+++ contracts/2_added_state_var.sol
@@ -4,4 +4,5 @@ pragma solidity >0.5.0 <=0.9.0;
 
 contract Organization {
     uint16 num_of_emp = 100; // This will be stored on blockchain and costs money
+    uint public total_Turnover = 1000000; //This variable will be accessible from outside env.
 }
